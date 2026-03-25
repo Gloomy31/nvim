9ebf7db5c574c8adb974fb2b02c07c844900a603
Gloomy <linkisawesome2364@gmail.com>
2026-03-25 13:25:08 -0500

    Set up mise

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,19 @@
 return {
     {
-        {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+        {
+            "nvim-treesitter/nvim-treesitter",
+            init = function()
+                require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+                    local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+                    local filename = vim.fn.fnamemodify(filepath, ":t")
+                    return string.match(filename, ".*mise.*%.toml$") ~= nil
+                end, { force = true, all = false })
+            end,
+            branch = 'master',
+            lazy = false,
+            build = ":TSUpdate"
+        },
+
         {"nvim-treesitter/nvim-treesitter-context"}
     }
 }
